5ea3ce9cb65b8b6c2e5f30fd700cf271603d1a07
jake adams <jakeadams1230@gmail.com>
2024-01-12 00:03:45 -0500

    test commit.

--- immerse/Core/Components/VideoPlayer.swift
+++ immerse/Core/Components/VideoPlayer.swift
@@ -20,7 +20,7 @@ struct VideoPlayerView: UIViewRepresentable {
         playerLayer.videoGravity = .resizeAspect
         view.layer.addSublayer(playerLayer)
 
-        return view
+        return view // this comment is for test commit
     }
 
     func updateUIView(_ uiView: UIView, context: Context) {
